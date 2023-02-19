747aecbf3182d48cc8e27fe663ba1d71722d6b90
MuhammadAmeerUsman <muhdusman0909@gmail.com>
2023-02-19 05:38:06 -0800

    task 1-9

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ *  *main - print the alphabet in lowercase and upper
+ *   *
+ *    *Description:Will print out the entire alphabet in lowercase,then inupper
+ *     *Return: 0
+ *
+ */
+int main(void)
+{
+		char a = 'a';
+		char z = 'A';
+
+		while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+
+		while (z <= 'Z')
+	{
+		putchar(z);
+		z++;
+	}
+
+		putchar('\n');
+		return (0);
+}
