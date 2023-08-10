d58030aee212760f5cb0fb58bb6994ab483b01ed
MuhammadAmeerUsman <muhdusman0909@gmail.com>
2023-08-10 14:17:52 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints a string using printf function
+ * Description: We are not allowed to use any otherr functions
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
