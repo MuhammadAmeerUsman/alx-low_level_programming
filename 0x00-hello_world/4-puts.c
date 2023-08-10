3170553a5a347de44b500834b1dfe17d2cf9bbef
MuhammadAmeerUsman <muhdusman0909@gmail.com>
2023-08-10 14:04:52 -0700

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Use puts function to print a string
+ * Description: We are not allowed to use any other function except puts
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	puts("/"Programming is like building a multilingual puzzle);
+
+	return (0);
+}
